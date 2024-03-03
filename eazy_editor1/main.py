597be1d8302aee139bc51fa7ce2c53e6d93c1fc9
crazyzay4 <ihar228gmail.com>
2024-03-03 13:08:15 +0200

    add choosing directory

---
+++ eazy_editor1/main.py
@@ -0,0 +1,38 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from ui import Ui_MainWindow
+import os 
+app = QApplication([])
+win = QMainWindow()
+
+ui = Ui_MainWindow()
+ui.setupUi(win)
+
+workdir = ""
+
+def chooseWorkDir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+    print(workdir)
+ui.dir_btn.clicked.connect(chooseWorkDir)
+
+def filter (files, extensions):
+    graphical_files = []
+    for file in files:
+        for ext in extensions:
+            if file.endswith(ext):
+                graphical_files.append(file)
+    return graphical_files
+
+def showFilenamesList():
+    extensions = [".png", ".jpg", ".jpeg", ".bmp", ".gif"]
+
+    filenames = os.listdir(workdir)
+    filename = filter(filenames, extensions)
+    print(filenames)
+    ui.files_list.clear()
+    ui.files_list.addItems(filenames)
+
+ui.dir_btn.clicked.connect(showFilenamesList)
+
+win.show()
+app.exec()
